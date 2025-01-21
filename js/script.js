bf9ce5073fdd56ac97efb8ee5a9c6d18c89d740e
faizanmanshad <officialfaizanmanshad@gmail.com>
2025-01-21 17:12:55 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -137,15 +137,8 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-
-
-
-
-
-
-
-
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click",function(){
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
